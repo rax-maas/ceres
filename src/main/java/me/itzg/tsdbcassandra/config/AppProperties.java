4fea0d84b28f8feb7e092bf6867b79163ed2b533
Geoff Bourne <itzgeoff@gmail.com>
2020-09-08 08:08:08 -0500

    Added AppProperties validation

--- src/main/java/me/itzg/tsdbcassandra/config/AppProperties.java
+++ src/main/java/me/itzg/tsdbcassandra/config/AppProperties.java
@@ -2,15 +2,19 @@ package me.itzg.tsdbcassandra.config;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.convert.DurationUnit;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties("app")
 @Component
 @Data
+@Validated
 public class AppProperties {
+  @NotNull
   @DurationUnit(ChronoUnit.SECONDS)
   Duration rawTtl = Duration.ofHours(6);
 }
