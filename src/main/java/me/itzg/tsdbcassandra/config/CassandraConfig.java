6a51ac3768df67f5ae2cc8fb1c2776f98ba89ede
Geoff Bourne <itzgeoff@gmail.com>
2020-09-11 22:44:40 -0500

    Configured ReactiveCqlTemplate with session factory

--- src/main/java/me/itzg/tsdbcassandra/config/CassandraConfig.java
+++ src/main/java/me/itzg/tsdbcassandra/config/CassandraConfig.java
@@ -1,24 +1,15 @@
 package me.itzg.tsdbcassandra.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.cassandra.ReactiveSession;
+import org.springframework.data.cassandra.ReactiveSessionFactory;
 import org.springframework.data.cassandra.core.cql.ReactiveCqlTemplate;
 
 @Configuration
 public class CassandraConfig {
 
-  private final CassandraProperties cassandraProperties;
-
-  @Autowired
-  public CassandraConfig(CassandraProperties cassandraProperties) {
-    this.cassandraProperties = cassandraProperties;
-  }
-
   @Bean
-  public ReactiveCqlTemplate cqlTemplate(ReactiveSession cqlSession) {
-    return new ReactiveCqlTemplate(cqlSession);
+  public ReactiveCqlTemplate cqlTemplate(ReactiveSessionFactory cqlSessionFactory) {
+    return new ReactiveCqlTemplate(cqlSessionFactory);
   }
 }
