855c5b25f66727b9656c4eb19064cd2703ab13fa
Geoff Bourne <itzgeoff@gmail.com>
2020-09-12 12:30:07 -0500

    Fixed TsdbCassandraApplicationTests

--- src/test/java/me/itzg/tsdbcassandra/TsdbCassandraApplicationTests.java
+++ src/test/java/me/itzg/tsdbcassandra/TsdbCassandraApplicationTests.java
@@ -2,9 +2,26 @@ package me.itzg.tsdbcassandra;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.CassandraContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class TsdbCassandraApplicationTests {
+	@Container
+	public static CassandraContainer<?> cassandraContainer = new CassandraContainer<>();
+	@TestConfiguration
+	@Import(CassandraContainerSetup.class)
+	public static class TestConfig {
+		@Bean
+		CassandraContainer<?> cassandraContainer() {
+			return cassandraContainer;
+		}
+	}
 
 	@Test
 	void contextLoads() {
