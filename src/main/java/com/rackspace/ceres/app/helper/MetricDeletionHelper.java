06bb6826826b165da8a1558ef3229d4b8ebc275f
Abhishek Kumar <abhishek.kumar1@rackspace.com>
2021-05-19 19:54:49 +0530

    made use of helper class to separate execution of cql queries.

---
+++ src/main/java/com/rackspace/ceres/app/helper/MetricDeletionHelper.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2021 Rackspace US, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.rackspace.ceres.app.helper;
+
+import com.github.benmanes.caffeine.cache.AsyncCache;
+import com.rackspace.ceres.app.config.AppProperties;
+import com.rackspace.ceres.app.model.SeriesSetCacheKey;
+import com.rackspace.ceres.app.services.DataTablesStatements;
+import java.time.Instant;
+import org.springframework.data.cassandra.core.cql.ReactiveCqlTemplate;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Component
+public class MetricDeletionHelper {
+
+  private static final String PREFIX_SERIES_SET_HASHES = "seriesSetHashes";
+  private static final String DELIM = "|";
+
+  private AppProperties appProperties;
+  private final DataTablesStatements dataTablesStatements;
+  private final ReactiveCqlTemplate cqlTemplate;
+  private final AsyncCache<SeriesSetCacheKey, Boolean> seriesSetExistenceCache;
+  private final ReactiveStringRedisTemplate redisTemplate;
+
+  public MetricDeletionHelper(AppProperties appProperties,
+      DataTablesStatements dataTablesStatements,
+      ReactiveCqlTemplate cqlTemplate, ReactiveStringRedisTemplate redisTemplate,
+      AsyncCache<SeriesSetCacheKey, Boolean> seriesSetExistenceCache) {
+    this.appProperties = appProperties;
+    this.dataTablesStatements = dataTablesStatements;
+    this.cqlTemplate = cqlTemplate;
+    this.seriesSetExistenceCache = seriesSetExistenceCache;
+    this.redisTemplate = redisTemplate;
+  }
+
+  public Mono<Boolean> deleteMetricNamesByTenantAndMetricName(String tenant, String metricName) {
+    return cqlTemplate
+        .execute("DELETE FROM metric_names WHERE tenant = ? AND metric_name = ?",
+            tenant, metricName)
+        .retryWhen(appProperties.getRetryDelete().build());
+  }
+
+  public Mono<Boolean> deleteSeriesSetsByTenantIdAndMetricName(String tenant,
+      String metricName) {
+    return cqlTemplate
+        .execute("DELETE FROM series_sets WHERE tenant = ? AND metric_name = ?"
+            , tenant, metricName)
+        .retryWhen(appProperties.getRetryDelete().build());
+  }
+
+  public Mono<Boolean> deleteSeriesSetHashes(String tenant, String seriesSetHash) {
+    return cqlTemplate
+        .execute("DELETE FROM series_set_hashes WHERE tenant = ? AND series_set_hash = ?",
+            tenant, seriesSetHash)
+        .retryWhen(appProperties.getRetryDelete().build());
+  }
+
+  public Mono<Boolean> deleteRawOrDownsampledEntries(String query, String tenant, Instant timeSlot,
+      String seriesSetHash) {
+    return cqlTemplate
+        .execute(query, tenant, timeSlot,
+            seriesSetHash)
+        .retryWhen(appProperties.getRetryDelete().build());
+  }
+
+  public Mono<Boolean> deleteRawOrDownsampledEntries(String query, String tenant,
+      Instant timeSlot) {
+    return cqlTemplate
+        .execute(query, tenant, timeSlot)
+        .retryWhen(appProperties.getRetryDelete().build());
+  }
+
+  public Mono<Boolean> removeEntryFromCache(String tenant, String seriesSetHash) {
+    seriesSetExistenceCache.synchronous()
+        .invalidate(new SeriesSetCacheKey(tenant, seriesSetHash));
+
+    return redisTemplate.delete(PREFIX_SERIES_SET_HASHES + DELIM + tenant + DELIM + seriesSetHash)
+        .flatMap(result -> {
+          if (result > 0) {
+            return Mono.just(true);
+          } else {
+            return Mono.just(false);
+          }
+        });
+  }
+
+  public Flux<String> getSeriesSetHashFromRaw(String tenant, Instant timeSlot) {
+    return cqlTemplate
+        .queryForFlux(dataTablesStatements.getRawGetHashQuery(), String.class, tenant, timeSlot);
+  }
+
+  public Flux<String> getSeriesSetHashFromSeriesSets(String tenant,
+      String metricName) {
+    return cqlTemplate.queryForFlux(
+        "SELECT series_set_hash FROM series_sets"
+            + " WHERE tenant = ? AND metric_name = ? ",
+        String.class,
+        tenant, metricName).distinct();
+  }
+}
--- src/main/java/com/rackspace/ceres/app/services/MetricDeletionService.java
+++ src/main/java/com/rackspace/ceres/app/services/MetricDeletionService.java
@@ -1,24 +1,36 @@
+/*
+ * Copyright 2021 Rackspace US, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.rackspace.ceres.app.services;
 
 
 import static com.rackspace.ceres.app.web.TagListConverter.convertPairsListToMap;
 
-import com.github.benmanes.caffeine.cache.AsyncCache;
-import com.rackspace.ceres.app.config.AppProperties;
 import com.rackspace.ceres.app.config.DownsampleProperties;
 import com.rackspace.ceres.app.config.DownsampleProperties.Granularity;
-import com.rackspace.ceres.app.model.SeriesSetCacheKey;
+import com.rackspace.ceres.app.helper.MetricDeletionHelper;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.cassandra.core.cql.ReactiveCqlTemplate;
-import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -29,37 +41,28 @@ import reactor.core.publisher.Sinks.Empty;
 @Profile("downsample")
 public class MetricDeletionService {
 
-  private static final String PREFIX_SERIES_SET_HASHES = "seriesSetHashes";
-  private static final String DELIM = "|";
 
-  private final ReactiveCqlTemplate cqlTemplate;
   private final DataTablesStatements dataTablesStatements;
   private final MetadataService metadataService;
-  private AppProperties appProperties;
   private TimeSlotPartitioner timeSlotPartitioner;
   private DownsampleProperties downsampleProperties;
-  private final ReactiveStringRedisTemplate redisTemplate;
-  private final AsyncCache<SeriesSetCacheKey, Boolean> seriesSetExistenceCache;
+  private final MetricDeletionHelper metricDeletionHelper;
 
   @Autowired
-  public MetricDeletionService(ReactiveCqlTemplate cqlTemplate,
-      DataTablesStatements dataTablesStatements, AppProperties appProperties,
+  public MetricDeletionService(DataTablesStatements dataTablesStatements,
       TimeSlotPartitioner timeSlotPartitioner, MetadataService metadataService,
-      DownsampleProperties downsampleProperties, ReactiveStringRedisTemplate redisTemplate,
-      AsyncCache<SeriesSetCacheKey, Boolean> seriesSetExistenceCache) {
-    this.cqlTemplate = cqlTemplate;
+      DownsampleProperties downsampleProperties,
+      MetricDeletionHelper metricDeletionHelper) {
     this.dataTablesStatements = dataTablesStatements;
-    this.appProperties = appProperties;
     this.timeSlotPartitioner = timeSlotPartitioner;
     this.downsampleProperties = downsampleProperties;
-    this.redisTemplate = redisTemplate;
     this.metadataService = metadataService;
-    this.seriesSetExistenceCache = seriesSetExistenceCache;
+    this.metricDeletionHelper = metricDeletionHelper;
   }
 
   public Mono<Empty> deleteMetrics(String tenant, String metricName, List<String> tag,
       Instant start, Instant end) {
-    if (StringUtils.isAllBlank(metricName)) {
+    if (StringUtils.isBlank(metricName)) {
       return deleteMetricsByTenantId(tenant, start, end);
     } else if (CollectionUtils.isEmpty(tag)) {
       return deleteMetricsByMetricName(tenant, metricName, start, end);
@@ -72,7 +75,7 @@ public class MetricDeletionService {
     log.debug("Deleting metrics for tenant: {}", tenant);
     return Flux.fromIterable(timeSlotPartitioner.partitionsOverRange(start, end, null))
         .flatMap(timeSlot ->
-            deleteMetricsByTenantId(downsampleProperties.getGranularities(), tenant, timeSlot))
+            deleteMetrics(downsampleProperties.getGranularities(), tenant, timeSlot))
         .then(Mono.empty());
   }
 
@@ -82,14 +85,14 @@ public class MetricDeletionService {
         tenant);
     return Flux.fromIterable(timeSlotPartitioner.partitionsOverRange(start, end, null))
         .flatMap(timeSlot -> {
-          Flux<String> seriesSetHashes = getSeriesSetHashFromSeriesSets(tenant,
+          Flux<String> seriesSetHashes = metricDeletionHelper.getSeriesSetHashFromSeriesSets(tenant,
               metricName);
-          return deleteMetric(downsampleProperties.getGranularities(), tenant, timeSlot, metricName,
+          return deleteMetrics(downsampleProperties.getGranularities(), tenant, timeSlot, metricName,
               seriesSetHashes);
         })
         //delete entries from metric_names table
         .flatMap(result ->
-            deleteMetricNamesByTenantAndMetricName(tenant, metricName)
+            metricDeletionHelper.deleteMetricNamesByTenantAndMetricName(tenant, metricName)
         )
         .then(Mono.empty());
   }
@@ -100,34 +103,45 @@ public class MetricDeletionService {
         "Deleting metrics {} with tag {} for tenant: {}  ",
         metricName, tag, tenant);
     Map<String, String> queryTags = convertPairsListToMap(tag);
+
     return Flux.fromIterable(timeSlotPartitioner.partitionsOverRange(start, end, null))
         .flatMap(timeSlot -> {
-          Flux<String> seriesSetHashes = metadataService.locateSeriesSetHashes(tenant,
-              metricName, queryTags);
-          return deleteMetric(downsampleProperties.getGranularities(), tenant, timeSlot, metricName,
-              seriesSetHashes);
+          return deleteMetrics(downsampleProperties.getGranularities(), tenant, timeSlot,
+              metricName,
+              metadataService.locateSeriesSetHashes(tenant, metricName, queryTags));
         }).then(Mono.empty());
   }
 
-  private Mono<Boolean> deleteMetricsByTenantId(List<Granularity> granularities, String tenant,
+  private Mono<Boolean> deleteMetrics(List<Granularity> granularities, String tenant,
       Instant timeSlot) {
-    return deleteMetricByTenant(granularities, tenant, timeSlot)
-        .then(deleteMetadataByTenant(tenant, timeSlot));
+    return deleteMetadataByTenantId(tenant, timeSlot)
+        .then(Flux.fromIterable(granularities)
+            .flatMap(granularity ->
+                metricDeletionHelper.deleteRawOrDownsampledEntries(dataTablesStatements
+                    .downsampleDelete(granularity.getWidth()), tenant, timeSlot))
+            .then(metricDeletionHelper.deleteRawOrDownsampledEntries(dataTablesStatements.getRawDelete(),
+                tenant, timeSlot)));
   }
 
-  private Mono<Boolean> deleteMetricByTenant(List<Granularity> granularities, String tenant, Instant timeSlot) {
+  private Mono<Boolean> deleteMetrics(List<Granularity> granularities, String tenant,
+      Instant timeSlot, String metricName, Flux<String> seriesSetHashes) {
+    return seriesSetHashes.flatMap(seriesSetHash ->
+        deleteMetricsByTenantIdAndSeriesSetHash(seriesSetHash, granularities, tenant, timeSlot)
+            .then(deleteMetadataByTenantIdAndSeriesSet(seriesSetHash, tenant, metricName)))
+        .then(Mono.just(true));
+  }
+
+  private Mono<Boolean> deleteMetricsByTenantIdAndSeriesSetHash(String seriesSetHash, List<Granularity> granularities, String tenant, Instant timeSlot) {
     return Flux.fromIterable(granularities)
         .flatMap(granularity ->
-            deleteRawOrDownsampledEntries(dataTablesStatements
-                .downsampleDelete(granularity.getWidth()), tenant, timeSlot))
-        .then(deleteRawOrDownsampledEntries(dataTablesStatements.getRawDelete(),
-            tenant, timeSlot));
+            metricDeletionHelper.deleteRawOrDownsampledEntries(dataTablesStatements.downsampleDeleteWithSeriesSetHash(granularity.getWidth()),
+                tenant, timeSlot, seriesSetHash))
+        .then(metricDeletionHelper.deleteRawOrDownsampledEntries(dataTablesStatements.getRawDeleteWithSeriesSetHash(), tenant, timeSlot, seriesSetHash));
   }
 
-
-  private Mono<Boolean> deleteMetadataByTenant(String tenant, Instant timeSlot) {
+  private Mono<Boolean> deleteMetadataByTenantId(String tenant, Instant timeSlot) {
     //get series set hashes from data raw table by tenant and timeSlot
-    Flux<String> seriesSetHashes = getSeriesSetHashFromRaw(tenant, timeSlot);
+    Flux<String> seriesSetHashes = metricDeletionHelper.getSeriesSetHashFromRaw(tenant, timeSlot);
     //get metricNames from metric_names table by tenant
     Flux<String> metricNames = metadataService.getMetricNames(tenant).flatMapMany(Flux::fromIterable);
 
@@ -136,105 +150,20 @@ public class MetricDeletionService {
             .then(Mono.just(true)));
   }
 
+  private Mono<Boolean> deleteMetadataByTenantIdAndSeriesSet(String seriesSetHash, String tenant, String metricName) {
+    return deleteSeriesSetHashesAndCache(tenant, seriesSetHash)
+        .then(metricDeletionHelper.deleteSeriesSetsByTenantIdAndMetricName(tenant, metricName));
+  }
+
+
   private Mono<Boolean> deleteSeriesSetHashesAndCache(String tenant, String seriesSetHash) {
-    return deleteSeriesSetHashes(tenant, seriesSetHash)
-        .then(removeEntryFromCache(tenant, seriesSetHash));
+    return metricDeletionHelper.deleteSeriesSetHashes(tenant, seriesSetHash)
+        .then(metricDeletionHelper.removeEntryFromCache(tenant, seriesSetHash));
   }
 
   private Mono<Boolean> deleteSeriesSetAndMetricName(String tenant, String metricName) {
-    return deleteSeriesSetsByTenantIdAndMetricName(tenant, metricName)
-        .then(deleteMetricNamesByTenantAndMetricName(tenant, metricName));
+    return metricDeletionHelper.deleteSeriesSetsByTenantIdAndMetricName(tenant, metricName)
+        .then(metricDeletionHelper.deleteMetricNamesByTenantAndMetricName(tenant, metricName));
   }
 
-
-  private Mono<Boolean> deleteMetricNamesByTenantAndMetricName(String tenant, String metricName) {
-    return cqlTemplate
-        .execute("DELETE FROM metric_names WHERE tenant = ? AND metric_name = ?",
-            tenant, metricName)
-        .retryWhen(appProperties.getRetryDelete().build());
-  }
-
-  private Mono<Boolean> deleteSeriesSetsByTenantIdAndMetricName(String tenant,
-      String metricName) {
-    return cqlTemplate
-        .execute("DELETE FROM series_sets WHERE tenant = ? AND metric_name = ?"
-            , tenant, metricName)
-        .retryWhen(appProperties.getRetryDelete().build());
-  }
-
-  private Mono<Boolean> deleteSeriesSetHashes(String tenant, String seriesSetHash) {
-    return cqlTemplate
-        .execute("DELETE FROM series_set_hashes WHERE tenant = ? AND series_set_hash = ?",
-            tenant, seriesSetHash)
-        .retryWhen(appProperties.getRetryDelete().build());
-  }
-
-  private Mono<Boolean> deleteRawOrDownsampledEntries(String query, String tenant, Instant timeSlot,
-      String seriesSetHash) {
-    return cqlTemplate
-        .execute(query, tenant, timeSlot,
-            seriesSetHash)
-        .retryWhen(appProperties.getRetryDelete().build());
-  }
-
-  private Mono<Boolean> deleteRawOrDownsampledEntries(String query, String tenant,
-      Instant timeSlot) {
-    return cqlTemplate
-        .execute(query, tenant, timeSlot)
-        .retryWhen(appProperties.getRetryDelete().build());
-  }
-
-  private Mono<Boolean> removeEntryFromCache(String tenant, String seriesSetHash) {
-    seriesSetExistenceCache.synchronous()
-        .invalidate(new SeriesSetCacheKey(tenant, seriesSetHash));
-
-    return redisTemplate.delete(PREFIX_SERIES_SET_HASHES + DELIM + tenant + DELIM + seriesSetHash)
-        .flatMap(result -> {
-          if (result > 0) {
-            return Mono.just(true);
-          } else {
-            return Mono.just(false);
-          }
-        });
-  }
-
-  private Flux<String> getSeriesSetHashFromRaw(String tenant, Instant timeSlot) {
-    return cqlTemplate
-        .queryForFlux(dataTablesStatements.getRawGetHashQuery(), String.class, tenant, timeSlot);
-  }
-
-  private Flux<String> getSeriesSetHashFromSeriesSets(String tenant,
-      String metricName) {
-    return cqlTemplate.queryForFlux(
-        "SELECT series_set_hash FROM series_sets"
-            + " WHERE tenant = ? AND metric_name = ? ",
-        String.class,
-        tenant, metricName).distinct();
-  }
-
-  private Mono<Boolean> deleteMetric(List<Granularity> granularities, String tenant,
-      Instant timeSlot, String metricName, Flux<String> seriesSetHashes) {
-    return
-        seriesSetHashes.flatMap(seriesSetHash -> delete(seriesSetHash, granularities, tenant, timeSlot, metricName))
-            .then(Mono.just(true));
-  }
-
-  private Mono<Boolean> delete(String seriesSetHash, List<Granularity> granularities, String tenant, Instant timeSlot, String metricName) {
-    return deleteMetricData(seriesSetHash, granularities, tenant, timeSlot)
-        .then(deleteMetadata(seriesSetHash, tenant, metricName));
-  }
-
-  private Mono<Boolean> deleteMetadata(String seriesSetHash, String tenant, String metricName) {
-    return deleteSeriesSetHashes(tenant, seriesSetHash)
-        .then(removeEntryFromCache(tenant, seriesSetHash))
-        .then(deleteSeriesSetsByTenantIdAndMetricName(tenant, metricName));
-  }
-
-  private Mono<Boolean> deleteMetricData(String seriesSetHash, List<Granularity> granularities, String tenant, Instant timeSlot) {
-    return Flux.fromIterable(granularities)
-        .flatMap(granularity ->
-            deleteRawOrDownsampledEntries(dataTablesStatements.downsampleDeleteWithSeriesSetHash(granularity.getWidth()),
-                tenant, timeSlot, seriesSetHash))
-        .then(deleteRawOrDownsampledEntries(dataTablesStatements.getRawDeleteWithSeriesSetHash(), tenant, timeSlot, seriesSetHash));
-  }
 }
