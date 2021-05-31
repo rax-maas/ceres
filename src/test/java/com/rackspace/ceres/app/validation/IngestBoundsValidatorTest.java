e7e65f14971a639703c094f8afc3dc7e8053e9a4
rohitsngh27 <rohitsngh27@gmail.com>
2021-05-31 17:30:40 +0530

    Added env var for actuator port

--- src/test/java/com/rackspace/ceres/app/validation/IngestBoundsValidatorTest.java
+++ src/test/java/com/rackspace/ceres/app/validation/IngestBoundsValidatorTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Rackspace US, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.rackspace.ceres.app.validation;
 
 import static java.time.temporal.ChronoUnit.DAYS;
@@ -9,7 +25,7 @@ import static org.mockito.Mockito.any;
 import com.rackspace.ceres.app.config.AppProperties;
 import java.time.Duration;
 import java.time.Instant;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class IngestBoundsValidatorTest {
 
