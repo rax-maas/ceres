440422ea242a12949afb8b8b3b476f4f810ff7e7
Mats Boman <mats.boman@rackspace.com>
2021-06-10 01:16:09 -0500

    Added a comment for update metric group query

--- src/main/java/com/rackspace/ceres/app/services/DataWriteService.java
+++ src/main/java/com/rackspace/ceres/app/services/DataWriteService.java
@@ -150,6 +150,9 @@ public class DataWriteService {
     return metadataService.metricGroupExists(tenant, metricGroup).flatMap(
         exists -> {
           if (exists) {
+            // Cassandra does not support NOT CONTAINS so we delete the metric name
+            // from the metric_names in case it already exists to avoid duplicates.
+            // If the metric name does not exists the delete will be ignored.
             return metadataService.updateMetricGroupRemoveMetricName(
                 tenant, metricGroup, metricName)
                 .and(metadataService.updateMetricGroupAddMetricName(
