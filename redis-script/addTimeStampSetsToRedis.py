9879ca6e752cd34392311baf11bdd4320c0dc015
Abhishek Kumar <abhishek.kumar1@rackspace.com>
2022-01-10 13:26:14 +0530

    replaced sadd with set

--- redis-script/addTimeStampSetsToRedis.py
+++ redis-script/addTimeStampSetsToRedis.py
@@ -3,15 +3,13 @@ import redis
 import sys
 from datetime import datetime, timedelta, timezone
 
-redisClient = redis.StrictRedis(host=str(sys.argv[1]), port=sys.argv[2], db=0, password=sys.argv[3])
-
 ts1 = datetime.now().astimezone().isoformat(timespec='milliseconds').replace('+00:00', 'Z')
 ts2 = (datetime.today() - timedelta(hours=0, minutes=1)).astimezone().isoformat(timespec='milliseconds').replace('+00:00', 'Z')
 ts3 = (datetime.today() - timedelta(hours=0, minutes=2)).astimezone().isoformat(timespec='milliseconds').replace('+00:00', 'Z')
 ts4 = (datetime.today() - timedelta(hours=0, minutes=3)).astimezone().isoformat(timespec='milliseconds').replace('+00:00', 'Z')
 
-
-redisClient.sadd(1, ts1)
-redisClient.sadd(2, ts2)
-redisClient.sadd(3, ts3)
-redisClient.sadd(4, ts4)
+redisClient = redis.StrictRedis(host=str(sys.argv[1]), port=sys.argv[2], db=0, password=sys.argv[3])
+redisClient.set("1", ts1)
+redisClient.set("2", ts2)
+redisClient.set("3", ts3)
+redisClient.set("4", ts4)
