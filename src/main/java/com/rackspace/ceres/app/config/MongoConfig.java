6ab929fef1b90828128ae1ad19487a8a9b63ecae
mats3569 <mats.boman@rackspace.com>
2022-06-02 10:36:38 -0500

    Updated handling of mongodb uri into config bean

--- src/main/java/com/rackspace/ceres/app/config/MongoConfig.java
+++ src/main/java/com/rackspace/ceres/app/config/MongoConfig.java
@@ -1,19 +1,32 @@
 package com.rackspace.ceres.app.config;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
+@Slf4j
 @Configuration
-public class MongoConfig
-        extends AbstractReactiveMongoConfiguration {
+public class MongoConfig extends AbstractReactiveMongoConfiguration {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongodbUri;
 
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create();
+        log.info("mongodb uri: {}", this.mongodbUri);
+        final ConnectionString connectionString =
+            new ConnectionString(this.mongodbUri);
+        final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+            .applyConnectionString(connectionString)
+            .build();
+        return MongoClients.create(mongoClientSettings);
     }
 
     @Override
