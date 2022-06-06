e3500212a674f75b1c32696b310fe9d16fc6787b
mats3569 <mats.boman@rackspace.com>
2022-06-06 12:47:22 -0500

    Removed mongo config for connection test

--- src/main/java/com/rackspace/ceres/app/config/MongoConfig.java
+++ src/main/java/com/rackspace/ceres/app/config/MongoConfig.java
@@ -9,32 +9,30 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @Slf4j
 @Configuration
-public class MongoConfig extends AbstractReactiveMongoConfiguration {
-
-    @Value("${spring.data.mongodb.uri}")
-    private String mongodbUri;
-
-    @Bean
-    public MongoClient mongoClient() {
-        log.info("mongodb uri: {}", this.mongodbUri);
-        final ConnectionString connectionString = new ConnectionString("mongodb://root:myU8KfkjLC@cassandra-mongodb/ceres?authSource=admin");
-        final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
-            .applyConnectionString(connectionString)
-            .build();
-        return MongoClients.create(mongoClientSettings);
-    }
-
-    @Override
-    protected String getDatabaseName() {
-        return "ceres";
-    }
-
-    @Override
-    protected boolean autoIndexCreation() {
-        return true;
-    }
+public class MongoConfig {
+//    @Value("${spring.data.mongodb.uri}")
+//    private String mongodbUri;
+//
+//    @Bean
+//    public MongoClient mongoClient() {
+//        log.info("mongodb uri: {}", this.mongodbUri);
+//        final ConnectionString connectionString = new ConnectionString("mongodb://root:myU8KfkjLC@cassandra-mongodb/ceres?authSource=admin");
+//        final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+//            .applyConnectionString(connectionString)
+//            .build();
+//        return MongoClients.create(mongoClientSettings);
+//    }
+//
+//    @Override
+//    protected String getDatabaseName() {
+//        return "ceres";
+//    }
+//
+//    @Override
+//    protected boolean autoIndexCreation() {
+//        return true;
+//    }
 }
