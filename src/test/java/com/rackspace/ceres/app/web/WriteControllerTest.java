5374f260e34dac4ce58779becb9203d9929ff242
mats3569 <mats.boman@rackspace.com>
2022-06-07 23:44:14 -0500

    Fixed write ccontroller test

--- src/test/java/com/rackspace/ceres/app/web/WriteControllerTest.java
+++ src/test/java/com/rackspace/ceres/app/web/WriteControllerTest.java
@@ -4,6 +4,7 @@ import com.rackspace.ceres.app.config.AppProperties;
 import com.rackspace.ceres.app.model.Metric;
 import com.rackspace.ceres.app.model.PutResponse;
 import com.rackspace.ceres.app.model.TagFilter;
+import com.rackspace.ceres.app.repos.DownsamplingRepository;
 import com.rackspace.ceres.app.services.DataWriteService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -39,6 +40,9 @@ public class WriteControllerTest {
 
   @MockBean
   DataWriteService dataWriteService;
+
+  @MockBean
+  DownsamplingRepository downsamplingRepository;
 
   @Autowired
   WebTestClient webTestClient;
