918ed86942b203f5646d3a1daca5c59a351e8ecb
mats3569 <mats.boman@rackspace.com>
2022-06-09 22:12:43 -0500

    Optim imports

--- src/main/java/com/rackspace/ceres/app/model/Timeslot.java
+++ src/main/java/com/rackspace/ceres/app/model/Timeslot.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.index.CompoundIndexes;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
