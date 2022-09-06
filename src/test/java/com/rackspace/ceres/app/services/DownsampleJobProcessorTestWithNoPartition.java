22c4d8d3963bef9e26307baccb4e77beee7f54f0
Abhishek Kumar <abhishek.kumar1@rackspace.com>
2022-09-06 18:07:19 +0530

    added testDownsampleJobProcessorWithNoPartition

--- src/test/java/com/rackspace/ceres/app/services/DownsampleJobProcessorTestWithNoPartition.java
+++ src/test/java/com/rackspace/ceres/app/services/DownsampleJobProcessorTestWithNoPartition.java
@@ -18,18 +18,14 @@
 package com.rackspace.ceres.app.services;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import com.rackspace.ceres.app.CassandraContainerSetup;
-import com.rackspace.ceres.app.model.PendingDownsampleSet;
+import com.rackspace.ceres.app.config.DownsampleProperties;
 import java.time.Instant;
 import java.util.concurrent.ScheduledExecutorService;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -46,11 +42,11 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @SpringBootTest(properties = {
-    "ceres.downsample.partitions=1"
+    "ceres.downsample.partitions=0"
 })
 @ActiveProfiles(profiles = {"test", "downsample"})
 @Testcontainers
-public class DownsampleJobProcessorTest {
+public class DownsampleJobProcessorTestWithNoPartition {
 
   @Container
   public static CassandraContainer<?> cassandraContainer = new CassandraContainer<>(
@@ -71,41 +67,24 @@ public class DownsampleJobProcessorTest {
   DownsampleTrackingService downsampleTrackingService;
 
   @Autowired
-  private ScheduledExecutorService executor;
+  ScheduledExecutorService executor;
 
   @MockBean
-  private DownsampleProcessor downsampleProcessor;
+  DownsampleProcessor downsampleProcessor;
 
   @Autowired
   DownsampleJobProcessor downsampleJobProcessor;
 
+  @Autowired
+  DownsampleProperties downsampleProperties;
+
   @Test
-  public void testDownsampleJobProcessor() throws InterruptedException {
+  public void testDownsampleJobProcessorWithNoPartition() {
     Instant now = Instant.now();
-
-    final String tenant = RandomStringUtils.randomAlphanumeric(10);
-    final String seriesSet = RandomStringUtils.randomAlphanumeric(10) + ",host=h-1";
-    final PendingDownsampleSet pendingSet = new PendingDownsampleSet()
-        .setSeriesSetHash(seriesSet)
-        .setTenant(tenant)
-        .setTimeSlot(now);
-
     when(downsampleTrackingService.claimJob(any(), anyString()))
         .thenReturn(Flux.just("free"));
     when(downsampleTrackingService.getTimeSlot(any(), anyString()))
         .thenReturn(Mono.just(String.valueOf(now.getEpochSecond())));
-    when(downsampleTrackingService.getDownsampleSets(anyLong(), anyInt()))
-        .thenReturn(Flux.just(pendingSet));
-    when(downsampleProcessor.processSet(any(PendingDownsampleSet.class), anyInt(), anyString(),
-        anyString()))
-        .thenReturn(Flux.empty());
-    when(downsampleTrackingService.deleteTimeslot(anyInt(), anyString(), anyLong()))
-        .thenReturn(Mono.empty());
-
-    verify(downsampleTrackingService, atLeast(1)).freeJob(0, "PT15M");
-    Thread.sleep(1 * 60 * 1000);
-    verify(downsampleTrackingService, atLeast(1)).freeJob(0, "PT15M");
-    verify(downsampleTrackingService, atLeast(1)).claimJob(0, "PT15M");
-    verify(downsampleTrackingService, atLeast(1)).getTimeSlot(0, "PT15M");
+    verifyNoInteractions(downsampleTrackingService);
   }
 }
