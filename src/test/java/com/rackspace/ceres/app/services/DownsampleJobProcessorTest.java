5f68f92c4bd20107693161ef66148e57b8d3beca
Abhishek Kumar <abhishek.kumar1@rackspace.com>
2022-09-06 15:53:14 +0530

    added test case for DownsampleJobProcessor

--- src/test/java/com/rackspace/ceres/app/services/DownsampleJobProcessorTest.java
+++ src/test/java/com/rackspace/ceres/app/services/DownsampleJobProcessorTest.java
@@ -18,15 +18,18 @@
 package com.rackspace.ceres.app.services;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.rackspace.ceres.app.CassandraContainerSetup;
+import com.rackspace.ceres.app.model.PendingDownsampleSet;
 import java.time.Instant;
 import java.util.concurrent.ScheduledExecutorService;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -77,14 +80,30 @@ public class DownsampleJobProcessorTest {
   DownsampleJobProcessor downsampleJobProcessor;
 
   @Test
-  public void testDownsampleJobProcessorMethods() throws InterruptedException {
+  public void testProcessTimeSlot() throws InterruptedException {
     Instant now = Instant.now();
-    when(downsampleTrackingService.claimJob(any(), anyString())).thenReturn(Flux.just("free"));
+
+    final String tenant = RandomStringUtils.randomAlphanumeric(10);
+    final String seriesSet = RandomStringUtils.randomAlphanumeric(10) + ",host=h-1";
+    final PendingDownsampleSet pendingSet = new PendingDownsampleSet()
+        .setSeriesSetHash(seriesSet)
+        .setTenant(tenant)
+        .setTimeSlot(now);
+
+    when(downsampleTrackingService.claimJob(any(), anyString()))
+        .thenReturn(Flux.just("free"));
     when(downsampleTrackingService.getTimeSlot(any(), anyString()))
         .thenReturn(Mono.just(String.valueOf(now.getEpochSecond())));
+    when(downsampleTrackingService.getDownsampleSets(anyLong(), anyInt()))
+        .thenReturn(Flux.just(pendingSet));
+    when(downsampleProcessor.processSet(any(PendingDownsampleSet.class), anyInt(), anyString(), anyString()))
+        .thenReturn(Flux.empty());
+    when(downsampleTrackingService.deleteTimeslot(anyInt(), anyString(), anyLong()))
+        .thenReturn(Mono.empty());
 
-    verify(downsampleTrackingService, times(1)).freeJob(0, "PT15M");
+    verify(downsampleTrackingService, atLeast(1)).freeJob(0, "PT15M");
     Thread.sleep(1*60*1000);
+    verify(downsampleTrackingService, atLeast(1)).freeJob(0, "PT15M");
     verify(downsampleTrackingService, atLeast(1)).claimJob(0, "PT15M");
     verify(downsampleTrackingService, atLeast(1)).getTimeSlot(0, "PT15M");
   }
